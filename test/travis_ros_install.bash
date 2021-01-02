07b01ec13e74a0a7732de5bb0bcb1fb7a215b445
Yoshinori Nagayama <test@gmail.com>
2021-01-02 09:32:03 +0000

    Add travis.yml and travis_ros_install.bash

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -xve
+
+#required packages
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
+
+#ros install
+cd ..
+git clone http://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
+bash ./setp0.bash
+bash ./step1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+source /opt/ros/indigo/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
